3930114f8a45d028ffe80d44227c028c4f0c60d9
oladetoungee <demilad1998@gmail.com>
2024-12-02 23:36:38 +0100

    env update

--- src/lib/utils/sendMail.ts
+++ src/lib/utils/sendMail.ts
@@ -22,8 +22,8 @@ export const sendMail = async ({
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      user: process.env.NEXT_PUBLIC_EMAIL_USER,
+      pass: process.env.NEXT_PUBLIC_EMAIL_PASS,
     },
   });
 
