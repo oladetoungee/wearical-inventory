1e4ffe5ca0427cb416ce2abe83b0d282832c5e61
oladetoungee <demilad1998@gmail.com>
2025-01-20 22:57:20 +0100

    create doughnut component

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -1,9 +1,10 @@
 import { DashboardLayout } from '@/components/layout';
  import { ChartLine, ChartBar } from '@/components/ui';
-
+import { DashboardHome } from '@/components/dashboard';
 export default function EmployeePage() {
   return (
     <DashboardLayout>
+        <DashboardHome />
 <ChartLine />
 <ChartBar />
   </DashboardLayout>
