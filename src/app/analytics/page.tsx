51d195c40021b2e85574e7d23c342e76a1e2cd11
oladetoungee <demilad1998@gmail.com>
2025-01-20 22:54:01 +0100

    feat/analytics-page

---
+++ src/app/analytics/page.tsx
@@ -0,0 +1,11 @@
+import { DashboardLayout } from '@/components/layout';
+ import { ChartLine, ChartBar } from '@/components/ui';
+
+export default function EmployeePage() {
+  return (
+    <DashboardLayout>
+<ChartLine />
+<ChartBar />
+  </DashboardLayout>
+  );
+}
